a155656fc6716d0eec81ca82553c46ed3bcf32eb
bondarev_md <bondarevmd@transport.mos.ru>
2025-10-18 00:25:13 +0300

    Initial commit

---
+++ src/main/java/ru/shibo/focusos/FocusosApplication.java
@@ -0,0 +1,13 @@
+package ru.shibo.focusos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocusosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocusosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/shibo/focusos/FocusosApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.shibo.focusos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FocusosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
